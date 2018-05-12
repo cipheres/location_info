38e7881b521c7e0d74f55c2c36c4dc04afa2031a
cipheres <cipheres@gmail.com>
2018-05-12 13:31:15 +0100

    INIt

---
+++ app/src/main/java/com/cipheres/locationinfo/AuthActivity.java
@@ -0,0 +1,12 @@
+package com.cipheres.locationinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
